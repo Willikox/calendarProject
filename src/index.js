bd3b64262a55ff845b00466f6b9374f973f5a856
Willikox <willedu94@gmail.com>
2024-07-05 07:39:32 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import getStore from "./store/getStore";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./sass/app.scss";
+import Calendar from "./pages/Calendar";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+const store = getStore();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <Calendar />
+    </ReduxProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import remindersReducer from "./reminders";
+import editReminderReducer from "./editReminder";
+
+const rootReducer = combineReducers({
+  reminders: remindersReducer,
+  editReminder: editReminderReducer,
+});
+
+export default rootReducer;
